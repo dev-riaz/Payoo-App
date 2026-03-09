1fb9a51fd626e60895868b1cb214f56e6e561f1c
riaz113 <riazmahamud113@gmail.com>
2026-03-09 19:41:18 +0600

    cash out section complet

--- js/cashout.js
+++ js/cashout.js
@@ -1,1 +1,33 @@
-console.log('jjjjjjjjj');
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    const agentNumber = document.getElementById('agent-number');
+    const agentNumberValue = agentNumber.value;
+    if (agentNumberValue.length !== 11) {
+        alert('invalid Agent Number');
+        return;
+    }
+
+    const cashoutAmount = document.getElementById('cashout-amount');
+    const cashoutAmountValue = cashoutAmount.value;
+    console.log(cashoutAmountValue);
+
+    const correctBalance = document.getElementById('balance');
+    const balance = correctBalance.innerText;
+    console.log(balance);
+    const newBalance = Number(balance) - Number(cashoutAmountValue);
+    if (newBalance < 0) {
+        alert('invalid Amount');
+        return;
+    }
+
+    const cashoutPin = document.getElementById('cashout-pin');
+    const cashoutPinValue = cashoutPin.value;
+    if (cashoutPinValue === '1234') {
+        alert('Withdraw Successful');
+        console.log(newBalance);
+        correctBalance.innerText = newBalance;
+
+    } else {
+        alert('invalid Pin')
+        return;
+    }
+})
